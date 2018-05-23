6b94954c0e75892180360db2c36f8f9757d0f6d0
Ahmad Ibrahim <ahmadmuhamo@gmail.com>
2018-05-23 12:12:01 +0200

    Added sticky navigation

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,11 @@
 console.log('App started!');
+
+window.onscroll = () => {
+    const navbar = document.querySelector("nav");
+    const header = document.querySelector('header').offsetHeight;
+    if (window.pageYOffset >= header) {
+        navbar.classList.add("sticky-nav")
+    } else {
+        navbar.classList.remove("sticky-nav");
+    }
+};
